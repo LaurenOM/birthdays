a0a15faf87ceede84995621c3646d3daef17c2da
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-15 12:05:11 +0100

    FINISHED BIRTHDAYS

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,16 +1,38 @@
 class BirthdayList
-	def store_birthday(name,date)
-		birthdays = []
 
-		birthdays << {name: name, date: date}
+	attr_reader :birthdays
+
+	def initialize
+		@birthdays = []
+	end
+
+	def store_birthday(name,date)
+
+		@birthdays << {name: name, date: date}
 
 		birthdays
 	end
-
-	def add_name(name)
-			
-	namelist = []
 	
-	namelist << name
+	def print_birthdays
+		@birthdays.each do |hash|
+			puts "#{hash[:name]}: #{hash[:date]}"
+		end
+	end
+	
+	def today
+		Time.now.strftime("%d-%m")
+	end 
+
+	def check_age
+		Time.now.strftime("%Y").to_i - @birthdays[0][:date][6..9].to_i
+	end
+
+
+	def birthdays_today
+		@birthdays.each do |hash|
+			if hash[:date][0..4] == today 
+				return "It's #{hash[:name]}'s birthday today! They are #{check_age} years old!"
+			end
+		end
 	end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,36 +1,50 @@
 require 'birthday_list'
+require 'timecop'
 
 describe BirthdayList do 
 	let (:birthday_list) { BirthdayList.new }
+
+	before do
+		Timecop.freeze(Time.local(2019,5,15))
+	  end
+	
+	  after do
+		Timecop.return
+	  end
 
 	it 'can recognise the Birthday_List class' do
 		expect(birthday_list).to be_a_kind_of(BirthdayList)
 	end 
 
-	# it 'can recognise the store_birthday method' do 
-	# 	expect(birthday_list).to respond_to(:store_birthday).with(2).arguments
-	# end
-
-	# it 'can store a birthday to the list' do
-	# 	birthday = "22-09-1991"
-
-	# 	updated_list = birthday_list.store_birthday(birthday)
-		
-	# 	expect(updated_list).to include("22-09-1991")
-	# end
-
-	# it 'can store a name to birthday' do 
-	# 	expect(birthday_list).to respond_to(:add_name).with(1).argument
-	# end
-
-	it 'can store a name to birthday' do 
-		name = "Lauren"
-		expect(birthday_list.add_name(name)).to include("Lauren")
-	end
-
 	it 'can store name and birthday in a hash' do
-		new_list = birthday_list.store_birthday("lauren","22-09-1991")
+		new_list = birthday_list.store_birthday("lauren","22-09")
 
-		expect(new_list).to eq([{name: "lauren", date: "22-09-1991" }])
+		expect(new_list).to eq([{name: "lauren", date: "22-09" }])
 	end
+	
+	it 'prints each name and birthday in a nice way' do 
+		
+		birthday_list.store_birthday("Lauren", "22-09-1991")
+		birthday_list.store_birthday("Nino", "19-12-1991")
+
+		expect{birthday_list.print_birthdays}.to output("Lauren: 22-09-1991\nNino: 19-12-1991\n").to_stdout
+	end 
+
+	it "can check for today's date" do 
+
+		expect(birthday_list.today).to eq("15-05")
+	end
+
+	it "can check for someone's age" do 
+
+		birthday_list.store_birthday("Lauren","15-05-1991")
+
+		expect(birthday_list.check_age).to eq(28)
+	end
+	it "can check who's birthday is today and return a message" do 
+
+		birthday_list.store_birthday("Lauren", "15-05-1991")
+
+		expect(birthday_list.birthdays_today).to eq("It's Lauren's birthday today! They are 28 years old!")
+	end 
 end
